77cd4285172f58632db8b67eca4144b88fb397c5
Marzan1810662 <hassanmarzan17@gmail.com>
2022-02-17 12:22:29 +0600

    total expense and balance calculation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('Savings Abacus - script added');
+
+function getInputValue(id) {
+    const value = parseFloat(document.getElementById('input-' + id).value);
+    return value;
+}
+
+function calculateValue() {
+    const income = getInputValue('income');
+    const foodExpense = getInputValue('food');
+    const rent = getInputValue('rent');
+    const clothsExpense = getInputValue('cloths');
+    const totalExpense = foodExpense + rent + clothsExpense;
+
+    if(totalExpense > income){
+        console.log('expenses can not be greater than income');
+    }
+    else{
+        document.getElementById('total-expenses').innerText = totalExpense.toFixed(2);
+    
+        const balance = income - totalExpense;
+        document.getElementById('balance').innerText = balance.toFixed(2);}
+}
+
+document.getElementById('btn-calculate').addEventListener('click',
+    function () {
+        calculateValue();
+    });
